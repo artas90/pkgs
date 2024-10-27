8224483df80955f40092e62d05d0d2e4cfb87d4c
Andrew Sokolov <artas90@gmail.com>
2024-10-28 00:08:53 +0200

    add windows version of yazi

--- .scripts/update-yazi.sh
+++ .scripts/update-yazi.sh
@@ -1,14 +1,23 @@
 cd `dirname $0` && cd .. && _rootdir=`pwd`
 
 update_yazi() {
-  local yaziver="$1"
-  local yaziarch="$2"
+  local ver="$1"
+  local arch="$2"
+  local os="$3"
 
-  wget https://github.com/sxyazi/yazi/releases/download/v$yaziver/yazi-$yaziarch-unknown-linux-musl.zip
-  unzip yazi-$yaziarch-unknown-linux-musl.zip
-  mv yazi-$yaziarch-unknown-linux-musl/yazi $_rootdir/yazi-$yaziver-$yaziarch-linux
-  rm -rf ./yazi-$yaziarch-unknown-linux-musl ./yazi-$yaziarch-unknown-linux-musl.zip
+  local pkg="yazi-${arch}-${os}"
+  local exe=""
+  if [ $os = "pc-windows-msvc" ]; then
+    exe=".exe"
+  fi
+
+  wget https://github.com/sxyazi/yazi/releases/download/v${ver}/${pkg}.zip
+  unzip ${pkg}.zip
+  mv "${pkg}/yazi${exe}" "$_rootdir/yazi-${ver}-${arch}-${os}${exe}"
+  rm -rf ./${pkg} ./${pkg}.zip
 }
 
-update_yazi "0.3.3" "x86_64"
-update_yazi "0.3.3" "aarch64"
+update_yazi "0.3.3" "x86_64"  "unknown-linux-musl"
+update_yazi "0.3.3" "x86_64"  "pc-windows-msvc"
+update_yazi "0.3.3" "aarch64" "unknown-linux-musl"
+update_yazi "0.3.3" "aarch64" "pc-windows-msvc"
