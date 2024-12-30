b4d4fa1d1978fee177dd6a8e1275f3a4be10dd1b
Andrew Sokolov <artas90@gmail.com>
2024-12-30 22:17:08 +0200

    add prefetched prezto

---
+++ .scripts/update-prezto.sh
@@ -0,0 +1,14 @@
+cd `dirname $0` && cd .. && _rootdir=`pwd`
+
+_update_prezto() {
+  local ver=`date +"%Y-%m-%d-%H-%M-%S"`
+  rm -rf "$_rootdir/prezto"
+  mkdir "$_rootdir/prezto"
+  echo -n "$ver" > "$_rootdir/prezto/VERSION"
+
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git ".zprezto"
+  tar --no-xattrs -cvzf "prezto/prezto-$ver.tgz" ".zprezto"
+
+  rm -rf ".zprezto"
+}
+_update_prezto
