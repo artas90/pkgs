cb0da8dd0903327943500a218c04a03705f922b9
Andrew Sokolov <artas90@gmail.com>
2025-10-28 18:40:04 +0200

    update micro to 2025.10.28

---
+++ micro/build.sh
@@ -0,0 +1,32 @@
+apt update && apt install -y build-essential curl git
+
+mver=`date +"%Y.%-m.%-d"`
+gver=`curl -s https://go.dev/VERSION?m=text | head -n 1 | sed 's/^go//'`
+arch=""
+case "$(uname -m)" in
+  x86_64)  arch="amd64" ;;
+  aarch64) arch="arm64" ;;
+  *)       echo "error: unknown architecture"; exit ;;
+esac
+
+cd /tmp
+
+# -- install deps -- -- --
+
+mkdir -p /tmp/golib
+gopkg="go$gver.linux-$arch.tar.gz"
+curl "https://storage.googleapis.com/golang/$gopkg" -o $gopkg
+tar -xzf $gopkg
+
+export PATH=$PATH:/tmp/go/bin
+export GOPATH=/tmp/golib
+
+# -- build app -- -- --
+
+git clone --depth=1 https://github.com/zyedidia/micro && cd /tmp/micro
+git tag v$mver && git checkout v$mver && make build
+
+# -- copy files -- -- --
+
+mv /tmp/micro/micro /app/micro-$mver-$(uname -m)
+echo $mver > /app/VERSION
